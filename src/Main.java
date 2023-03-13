a69f236a7f8f22ec2b72015b96b8264d4fee9f6c
Ansar Ismagilov <ansar89kab@bk.ru>
2023-03-13 11:36:08 +0700

    DONE MY SOLUTION

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {8,0,7,1,7,7,9,7,5,2,9,1,7,3,7,0,6,5,1,7,7,9,3,8,1,5,7,7,8,4,0,9,3,7,3,4,5,7,4,8,8,5,8,9,8,5,8,8,4,7,5,4,3,7,3,9,0,4,8,7,7,5,1,8,3,9,7,7,1,5,6,0,7,3,7,1,9,2,5,7,9,7,7,1,7,0,8};
+        ListNode next = null;
+        ListNode lNcurrent = new ListNode(nums[nums.length - 1],null);
+        for (int i = nums.length - 2; i >=0 ; i--) {
+            next = lNcurrent;
+            lNcurrent = new ListNode(nums[i],next);
+        }
+
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(lNcurrent));
     }
 }
---
+++ src/Solution.java
@@ -0,0 +1,40 @@
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Comparator;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayDeque<Integer>
+                deque1 = new ArrayDeque<>(),
+                deque2 = new ArrayDeque<>();
+
+        while (head != null) {
+            deque1.addFirst(head.val);
+            deque2.addLast(head.val);
+            head = head.next;
+        }
+        boolean result = false;
+        while (!deque1.isEmpty() && !deque2.isEmpty()) {
+            if (deque1.pop() != deque2.pop()) {result = false; break;}
+            else result = true;
+        }
+        return result;
+    }
+}
